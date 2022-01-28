99cf75ac1990955bd4097893a02fbb7c2dc9041f
Joe Izang <josephizang@gmail.com>
2022-01-29 00:54:37 +0100

    add template project and add nugets. change and add example endpoint

---
+++ sample/Templates/AspNetCoreApiEndpoints/Endpoints/WeatherForecastEndpoint/GetAll.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace AspNetCoreApiEndpoints.Endpoints.WeatherForecastEndpoint;
+
+public class GetAll : EndpointBaseSync
+  .WithoutRequest
+  .WithActionResult<IEnumerable<WeatherForecast>>
+{
+  private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+  [HttpGet("api/weatherforecasts")]
+  [SwaggerOperation(
+    Summary = "List all Weather Forecasts",
+    Description = "List all Weather Forecasts",
+    OperationId = "Weather.List",
+    Tags = new[] { "WeatherForecastEndpoint" }
+    )]
+  public override ActionResult<IEnumerable<WeatherForecast>> Handle()
+  {
+    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    {
+      Date = DateTime.Now.AddDays(index),
+      TemperatureC = Random.Shared.Next(-20, 55),
+      Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    })
+            .ToArray();
+  }
+}
---
+++ sample/Templates/AspNetCoreApiEndpoints/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(s =>
+{
+  s.SwaggerDoc("v1", new OpenApiInfo { Title = "Sample Api", Version = "v1" });
+  s.EnableAnnotations();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
